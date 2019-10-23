d0928765e316702a76bc44eb217fc5bf808e3e18
Ariel Martínez Silberstein <amartinez5@uc.cl>
2019-10-23 16:23:57 -0300

    :children_crossing: add complete user post experience (#3)

--- dondeestas/views.py
+++ dondeestas/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
+import requests
+from urllib.parse import parse_qsl
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from .forms import RegisterForm
+
+BASE_URL = "http://donde-estas.herokuapp.com"
 
 
 def get_landing_page(request):
@@ -10,5 +15,14 @@ def get_missing_list(request):
     return HttpResponse(render(request, "missing.html"))
 
 
-def get_register_form(request):
-    return HttpResponse(render(request, "register.html"))
+def process_registration_view(request):
+    if request.method != "POST":
+        ctx = {"form": RegisterForm()}
+        return HttpResponse(render(request, "register.html", context=ctx))
+    if RegisterForm(request.POST).is_valid():
+        query_params = request.body.decode()
+        response = requests.post(f"{BASE_URL}/person?{query_params}")
+        print(response, response.json())  # for debugging
+        if response.ok and response.json()["success"]:
+            return JsonResponse({"success": True})
+    return JsonResponse({"success": False})
