0034e25e1e5c0d6009da28e81667f328dffba3a1
Daniel Leal <dlleal@uc.cl>
2019-10-23 00:54:49 -0300

    Mi vieja mula no es lo que era

--- static/js/register.js
+++ static/js/register.js
@@ -1,31 +1,32 @@
 /* eslint-disable no-undef */
 $(document).ready(() => {
-  // const container = $('div#register');
-  const form = $('#register');
 
-  form.submit((event) => {
+  var frm = $('#register');
+
+  var form = {}
+
+  jQuery.each(frm.serializeArray(), function() {
+    form[this.name] = this.value;
+  });
+
+  console.log(form);
+
+  frm.submit((event) => {
     event.preventDefault();
-
-    function onSuccess(data) {
-      console.log('success');
-      console.log(data);
-    }
-
-    function onError(data) {
-      console.log('error');
-      console.log(data);
-    }
-
     $.ajax({
       type: 'POST',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
       url: 'http://donde-estas.herokuapp.com/missing',
-      dataType: 'jsonp',
-      data: form.serialize(),
-      success: onSuccess,
-      error: onError,
+      data: form,
+      success: function (data) {
+        console.log("Success");
+        console.log(JSON.parse(data.responseText));
+      },
+      error: function(data) {
+        console.log("Mi vieja mula ya no es lo que era");
+        console.log(JSON.parse(data.responseText));
+      }
     });
+    return false;
   });
+
 });
