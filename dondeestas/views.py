46db835a7bf4efbb272dd7132fbfe4253be35da8
Ariel Martínez <amartinez5@uc.cl>
2019-10-22 23:30:51 -0300

    :sparkles: add frontend person features

--- dondeestas/views.py
+++ dondeestas/views.py
@@ -1,8 +1,14 @@
-from django.views import View
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
-class LandingPageView(View):
-    def get(self, request):
-        return HttpResponse(render(request, "index.html"))
+def get_landing_page(request):
+    return HttpResponse(render(request, "index.html"))
+
+
+def get_missing_list(request):
+    return HttpResponse(render(request, "missing.html"))
+
+
+def get_register_form(request):
+    return HttpResponse(render(request, "register.html"))
