dc8322e414745e5df175e7f0fb14d5d8aec0934b
Ariel Martínez Silberstein <amartinez5@uc.cl>
2019-10-24 00:40:17 -0300

    Revert ":beers: add post method"
    This reverts commit c58b7212392a08f6c0f3ba3b04fbd0f3003d80c4.

--- static/js/register.js
+++ static/js/register.js
@@ -1,20 +1,29 @@
 /* eslint-disable no-undef */
 $(document).ready(() => {
-  var form = $('#register');
 
-  form.submit((event) => {
+  var frm = $('#register');
+
+  var form = {}
+
+  jQuery.each(frm.serializeArray(), function() {
+    form[this.name] = this.value;
+  });
+
+  console.log(form);
+
+  frm.submit((event) => {
     event.preventDefault();
-
     $.ajax({
       type: 'POST',
-      url: `http://donde-estas.herokuapp.com/person?${form.serialize()}`,
-      // data: jQuery.param(form),
+      url: 'http://donde-estas.herokuapp.com/missing',
+      data: form,
       success: function (data) {
         console.log("Success");
-        console.log(JSON.parse(JSON.stringify(data)));
+        console.log(JSON.parse(data.responseText));
       },
       error: function(data) {
-        console.log(JSON.parse(JSON.stringify(data)));
+        console.log("Mi vieja mula ya no es lo que era");
+        console.log(JSON.parse(data.responseText));
       }
     });
     return false;
