4f903e9784644666f1a2d0000cd9a71b4a69abff
Ariel Martínez Silberstein <amartinez5@uc.cl>
2019-10-24 00:40:17 -0300

    Revert "People View (#4)"
    This reverts commit dfe8198aee0d0a389e7cccc8cd54831d27b7fbf1.

--- dondeestas/views.py
+++ dondeestas/views.py
@@ -1,34 +1,27 @@
 import requests
+from urllib.parse import parse_qsl
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from .forms import RegisterForm
-from django.conf import settings
 
-CARDS_PER_LINE = 3
+BASE_URL = "http://donde-estas.herokuapp.com"
 
 
 def get_landing_page(request):
     return HttpResponse(render(request, "index.html"))
 
 
-def get_people(request):
-    people = requests.get(f"{settings.BASE_API_URL}/missing").json()
-    payload = people["payload"] if people["success"] else []
-    padding = [None] * (len(payload) % CARDS_PER_LINE)
-    ctx = {
-        "people": payload + padding,
-        "cards_per_line": CARDS_PER_LINE,
-    }
-    return HttpResponse(render(request, "people.html", ctx))
+def get_missing_list(request):
+    return HttpResponse(render(request, "missing.html"))
 
 
 def process_registration_view(request):
     if request.method != "POST":
         ctx = {"form": RegisterForm()}
-        return HttpResponse(render(request, "register.html", ctx))
+        return HttpResponse(render(request, "register.html", context=ctx))
     if RegisterForm(request.POST).is_valid():
         query_params = request.body.decode()
-        response = requests.post(f"{settings.BASE_API_URL}/person?{query_params}")
+        response = requests.post(f"{BASE_URL}/person?{query_params}")
         print(response, response.json())  # for debugging
         if response.ok and response.json()["success"]:
             return JsonResponse({"success": True})
