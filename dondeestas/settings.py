95387a553d86add60592708206a4c6af9207f9e9
Ariel Martínez Silberstein <amartinez5@uc.cl>
2019-10-24 00:40:17 -0300

    Revert ":sparkles: add frontend person features"
    This reverts commit 591e72ceb29e5dd87706949fc3a5968ce4d74464.

--- dondeestas/settings.py
+++ dondeestas/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "webpack_loader",
-    "django_static_jquery",
 ]
 
 MIDDLEWARE = [
--- dondeestas/views.py
+++ dondeestas/views.py
@@ -1,14 +1,8 @@
+from django.views import View
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def get_landing_page(request):
-    return HttpResponse(render(request, "index.html"))
-
-
-def get_missing_list(request):
-    return HttpResponse(render(request, "missing.html"))
-
-
-def get_register_form(request):
-    return HttpResponse(render(request, "register.html"))
+class LandingPageView(View):
+    def get(self, request):
+        return HttpResponse(render(request, "index.html"))
