016c83621420288dd3b3e9e0c27bedb13201fbf9
Ariel Martínez <amartinez5@uc.cl>
2019-10-23 23:58:05 -0300

    :sparkles: show real people data in people view
    send request to API and received data to view

--- dondeestas/views.py
+++ dondeestas/views.py
@@ -5,20 +5,29 @@ from django.shortcuts import render
 from .forms import RegisterForm
 
 BASE_URL = "http://donde-estas.herokuapp.com"
+CARDS_PER_LINE = 3
 
 
 def get_landing_page(request):
     return HttpResponse(render(request, "index.html"))
 
 
-def get_missing_list(request):
-    return HttpResponse(render(request, "missing.html"))
+def get_people(request):
+    print("HEHE")
+    people = requests.get(f"{BASE_URL}/missing").json()
+    payload = people["payload"] if people["success"] else []
+    padding = [None] * (len(payload) % CARDS_PER_LINE)
+    ctx = {
+        "people": payload + padding,
+        "cards_per_line": CARDS_PER_LINE,
+    }
+    return HttpResponse(render(request, "people.html", ctx))
 
 
 def process_registration_view(request):
     if request.method != "POST":
         ctx = {"form": RegisterForm()}
-        return HttpResponse(render(request, "register.html", context=ctx))
+        return HttpResponse(render(request, "register.html", ctx))
     if RegisterForm(request.POST).is_valid():
         query_params = request.body.decode()
         response = requests.post(f"{BASE_URL}/person?{query_params}")
