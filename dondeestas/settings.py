0bba39021acee08566ab4021ec4f052afd0f5b4c
Daniel Leal <dlleal@uc.cl>
2019-10-26 03:54:50 -0300

    Corrected some settings

--- dondeestas/settings.py
+++ dondeestas/settings.py
@@ -25,9 +25,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = int(os.getenv("DEBUG", 0))
 
-ALLOWED_HOSTS = ["localhost", "0.0.0.0"]
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+WEBAPP_URL = os.getenv("WEBAPP_URL", "*.herokuapp.com")
+
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", WEBAPP_URL]
 
 
 # Application definition
@@ -110,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Webpack
